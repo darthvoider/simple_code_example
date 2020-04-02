1581036263235bb39d94a2d033073493f2384283
Kostya Voider <voiderok@gmail.com>
2020-04-02 22:30:53 +0300

    upd menu item state

--- src/containers/sidebar-menu-item.js
+++ src/containers/sidebar-menu-item.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { Icon } from '../components/icon/icon';
 import useWindowDimensions from '../hooks/use-window-dimensions';
-import {setActiveItemId, closeSidebar} from '../store/actions';
+import { setActiveItemId, closeSidebar } from '../store/actions';
 
-const SidebarMenuItemConnected = ({item, setActiveItemId, closeSidebar}) => {
+const SidebarMenuItemConnected = ({ item, setActiveItemId, closeSidebar, activeItemId }) => {
     const { width } = useWindowDimensions();
+    const sidebarMenuItemClasses = classNames('sidebar-menu-item', {
+        'active': item.id === activeItemId
+    });
     const menuItemClickHandler = () => {
         setActiveItemId(item.id);
         if (width < 768) {
@@ -14,7 +18,7 @@ const SidebarMenuItemConnected = ({item, setActiveItemId, closeSidebar}) => {
         }
     };
     return (
-        <li className='sidebar-menu-item' onClick={menuItemClickHandler}>
+        <li className={sidebarMenuItemClasses} onClick={menuItemClickHandler}>
             <Icon name='active' className='icon-primary' />
             <span className='sidebar-menu-item-title'>{item.name}</span>
             <Icon name='arrow-right' />
@@ -22,12 +26,16 @@ const SidebarMenuItemConnected = ({item, setActiveItemId, closeSidebar}) => {
     );
 };
 
+const mapStateToProps = state => ({
+    activeItemId: state.activeItemId
+});
+
 const mapDispatchToProps = {
     setActiveItemId,
     closeSidebar
 };
 
-export const SidebarMenuItem = connect(null, mapDispatchToProps)(SidebarMenuItemConnected);
+export const SidebarMenuItem = connect(mapStateToProps, mapDispatchToProps)(SidebarMenuItemConnected);
 
 SidebarMenuItemConnected.propTypes = {
     item: PropTypes.object.isRequired
